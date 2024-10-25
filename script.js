bdc3ee0d0466023120ab84c064491a22f475a8ef
niltonrodri <junnior.ex@gmail.com>
2024-10-25 02:11:00 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const botao = document.querySelector('.bota-menu')
-const menuLateral = document.querySelector('.menu-lateral')
-const conteudo = document.querySelector('.conteudo')
-const background = document.querySelector('.background')
-
-botao.addEventListener(funcion)
-    console.log("Teste da nova função")
-}
