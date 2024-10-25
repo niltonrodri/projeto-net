c4b0171b23d4b400df92bf8778697332ef589186
niltonrodri <junnior.ex@gmail.com>
2024-10-25 02:08:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector('.bota-menu')
+const menuLateral = document.querySelector('.menu-lateral')
+const conteudo = document.querySelector('.conteudo')
+const background = document.querySelector('.background')
+
+botao.addEventListener(funcion)
+    console.log("Teste da nova função")
+}
